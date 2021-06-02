79310396ef18b25859510a0d1b21e9da4aa2b283
Douglas A.C <dac17@inf.ufpr.br>
2021-06-02 14:40:11 -0300

    Lendo entrada de problema e colocando em dicionário "problema".

--- lp_tempo/main.py
+++ lp_tempo/main.py
@@ -1,6 +1,35 @@
+import sys
+
+TEMPO_MAXIMO = 540
+
+def constroiEstruturas(tempo_total):
+    problema = {}
+    
+    #Definindo Tempo Total
+    problema["tempo_total"] = tempo_total
+    
+    #Lendo Entradas
+    lines = [line.strip() for line in sys.stdin.readlines()]
+    line_words = lines[0].split()
+
+    problema['qtn_maquinas'] = int(line_words[0])
+    problema['qtn_tempos'] = int(line_words[1])
+
+    problema['pedidos'] = []
+    for index,line in enumerate(lines[1:]):
+        if (index >= problema['qtn_tempos']):
+            exit(f'Existem mais tempos do que o especificado. (Maximo: {problema["qtn_tempos"]})')
+            
+        line_words = line.split()
+        problema['pedidos'].append((int(line_words[0]), int(line_words[1])))
+
+    return problema
 
 def main():
-    print("Hello World")
+    
+    # Armazenando Valores de Entrada do problema
+    problema = constroiEstruturas(TEMPO_MAXIMO)
+    print(problema)
 
 
 
