be7a750cd3ecdf18bbb9a23cb7c6cf19bf2ed7ef
Douglas A.C <dac17@inf.ufpr.br>
2021-07-05 17:04:45 -0300

    Modularizando código para solução.

--- Trabalho_2/bb_caminhada/main.py
+++ Trabalho_2/bb_caminhada/main.py
@@ -1,102 +1,9 @@
 #TODO: Remover linhas com "# PRINTDEBUG"
 
+
 import sys
-from pprint import pprint
 from datetime import datetime
-
-#  Verifica se valor de 'palavra' pode ser interpretado como interiro. Caso verdadeiro retorna.
-def verificaTransformacao(palavra): 
-  try:
-    return int(palavra)
-  except ValueError:
-    exit(f'ERRO: "{palavra}" não é um inteiro válido')
-
-class Problema:
-
-  def __init__(self):
-    self.vertices = 0
-    self.grafo = []
-    self.cores = []
-    self.maiorCicloISmples = []
-    self.tamanhoMaiorCicloSimples = 0
-    self.nosArvore = 0
-    self.tempo = 0
-
-  def setVertices(self, vertices):
-    if (vertices <= 0):
-      exit(f'ERRO: Quatidade de vérices deve ser maior que 0.')
-
-    self.vertices = vertices
-    self.grafo = [[0 for col in range(vertices)] for row in range(vertices)]
-    self.cores = [1] * vertices
-
-  def verificaEAdicionaAresta(self, v_ori, v_dest, valor):
-    #Verifica se peso é positivo
-    if(valor < 0 ):
-      exit(f'ERRO: Distância \'{valor}\' inválida.')
-    #Verifica se v_ori é um vértice válido.
-    if(v_ori < 0 or v_ori >= self.vertices):
-      exit(f'ERRO: Vertice \'{v_ori}\' inválido. (somente de 0 à {self.vertices - 1})')
-
-    #Verifica se v_dest é um vértice válido.
-    if(v_dest < 0 or v_dest >= self.vertices):
-      exit(f'ERRO: Vertice \'{v_dest}\' inválido. (somente de 0 à {self.vertices - 1})')
-    
-    # Efetua adição de aresta
-    self.adicionaAresta(v_ori, v_dest, valor)
-
-  # Adiciona Aresta a matriz de adjacência 'grafo'.
-  def adicionaAresta(self, v_ori, v_dest, valor):
-    # Adiciona valor a matrix de adjacencia(simétrica).
-    self.grafo[v_ori][v_dest] = self.grafo[v_dest][v_ori] = valor
-
-  # Remove Aresta a matriz de adjacência 'grafo'.
-  def removeAresta(self, v_ori, v_dest):
-    # Adiciona valor a matrix de adjacencia(simétrica).
-    valor = self.grafo[v_ori][v_dest]
-    self.grafo[v_ori][v_dest] = self.grafo[v_dest][v_ori] = 0
-    return valor
-
-  # aplica cor a vertice de posição 'vertice' (1 para visitado e 0 para não visitado.)
-  def mudarCor(self, vertice, cor):
-    self.cores[vertice] = cor
-
-  def imprimeProblema(self):
-    print(self.vertices)
-    pprint(self.grafo)
-    pprint(self.cores)
-
-
-# Lê entrada e define estruturas de problema.
-def constroiProblema(problema):
-
-  #Lendo Entradas
-  linhas = [linha.strip() for linha in sys.stdin.readlines()]
-  palavas_linha = linhas[0].split()
-
-  #Definindo Número de Vértices.
-  problema.setVertices(verificaTransformacao(palavas_linha[0]))
-
-  # Verifica se quantidade de linhas de pesos é igual a quantidade de vertices -1.
-  if(len(linhas[1:]) < (problema.vertices - 1)):
-    exit(f'ERRO: Quantidade list de pesos informada (\'{len(linhas[1:])}\') inferior a esperada(\'{(problema.vertices - 1)}\').')
-
-  # Percorre todas as linhas de pesos.
-  for v_ori, linha in enumerate(linhas[1:]):
-
-    palavas_linha = linha.split()
-
-    # Verifica se quantidade de pesos é compativel a vertice v_ori.
-    if(len(palavas_linha) != (problema.vertices - v_ori - 1)):
-      exit(f'ERRO: Quantidade informada de pesos inválida (para vertice \'{v_ori}\' somente \'{len(palavas_linha)}\' pesos indicados).')
-
-    # Percorre pesos, adicionando-os a valores de aresta.
-    for v_dest, palavra in enumerate(palavas_linha):
-      destino = v_ori + v_dest + 1
-      valor = verificaTransformacao(palavra)
-      problema.verificaEAdicionaAresta(v_ori, destino, valor)
-
-  return problema
+from problema import Problema, constroiProblema 
   
 #----------------------------
 #       CLASSICO
@@ -176,7 +83,7 @@ def buscaProdundidadeBB(problema, vertice, total, caminho):
     caminho.pop()
     return  
 
-  # Bloqueia visitas à 'vertice' (cores[vertice] = 1), garantindo ciclo simples.
+  # Bloqueia visitas à 'vertice' (cores[vertice] = 0), garantindo ciclo simples.
   problema.mudarCor(vertice, 0)
 
   # Percorrer linha da matriz correspondente a 'vertice'
@@ -197,13 +104,14 @@ def buscaProdundidadeBB(problema, vertice, total, caminho):
       total -= peso
       problema.adicionaAresta(vertice, idx_vizinho, peso)
 
-  # Libera visitas à 'vertice' (cores[vertice] = 0).
+  # Libera visitas à 'vertice' (cores[vertice] = 1).
   problema.mudarCor(vertice, 1)
 
   # Remove 'vertice' de caminho.
   caminho.pop()
 
-def resolverProblemaClassico(problema):
+
+def resolverProblema(problema):
 
   # problema.imprimeProblema() # PRINTDEBUG
 
@@ -257,7 +165,7 @@ def resolverProblemaClassico(problema):
 
   print("---------------------") # PRINTDEBUG
 
-  
+
 def main():
   #Inicializando Problema
   problema = Problema()
@@ -266,7 +174,8 @@ def main():
   constroiProblema(problema)
 
   # Resolve Problema !!!
-  resolverProblemaClassico(problema)
+  resolverProblema(problema)
+  
 
 if __name__ == "__main__":
   main()
