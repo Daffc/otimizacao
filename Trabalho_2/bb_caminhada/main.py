ceda80eb577eeba05dd831a321e48f3b31531c44
Douglas A.C <dac17@inf.ufpr.br>
2021-07-05 19:00:55 -0300

    Concertando váricas coisas para Branch and Buound.

--- Trabalho_2/bb_caminhada/main.py
+++ Trabalho_2/bb_caminhada/main.py
@@ -3,6 +3,7 @@
 
 import sys
 from datetime import datetime
+from pprint import pprint
 from problema import Problema, constroiProblema 
   
 #----------------------------
@@ -53,13 +54,20 @@ def buscaProdundidadeClassica(problema, vertice, total, caminho):
 #----------------------------
 
 #Percorre matriz de adjacência, somando valor para arestas cujos vetores ainda não foram visitados(cor = 1)
-def boundSomaArestasValidas(problema):
-  soma = 0
+def boundSomaArestasValidas(problema, total):
+  soma = total
+
   # pprint(problema.grafo) # PRINTDEBUG
+  # print(problema.cores) # PRINTDEBUG
   for idx, linha_adj in enumerate(problema.grafo):
-      for peso in linha_adj[idx:]:
-        soma += peso * problema.cores[idx]
-  
+    maximo = 0
+    for peso in linha_adj[idx:]:
+      aux = peso * problema.cores[idx]
+      if(aux > maximo):
+        maximo = aux      
+    soma += maximo
+    # print("\n")
+  # print(total, soma, problema.tamanhoMaiorCicloSimples)
   # print("***", soma) # PRINTDEBUG
   return soma 
 
@@ -91,81 +99,18 @@ def buscaProdundidadeBB(problema, vertice, total, caminho):
     # Caso exista aresta e vetor não foi visitado.
     if((peso>0) and problema.cores[idx_vizinho]):
       
+
+      problema.removeAresta(vertice, idx_vizinho)
+      total += peso
       # Caso caso não seja possível ultrapassar valor atual de caminho máximo, retornar(poda)
-      if(boundSomaArestasValidas(problema) < problema.tamanhoMaiorCicloSimples):
+      if(boundSomaArestasValidas(problema, total) <= problema.tamanhoMaiorCicloSimples):
         # pprint(problema.grafo) # PRINTDEBUG
         # print('\n') # PRINTDEBUG
+        # print("caminho:", caminho) # PRINTDEBUG
         problema.mudarCor(vertice, 0)
         caminho.pop()
         return
-      problema.removeAresta(vertice, idx_vizinho)
-      total += peso
       buscaProdundidadeBB(problema, idx_vizinho, total, caminho)
-      total -= peso
-      problema.adicionaAresta(vertice, idx_vizinho, peso)
-
-  # Libera visitas à 'vertice' (cores[vertice] = 1).
-  problema.mudarCor(vertice, 1)
-
-  # Remove 'vertice' de caminho.
-  caminho.pop()
-
-#----------------------------
-#       Branch and Bound 2
-#----------------------------
-
-#Percorre matriz de adjacência, somando valor para arestas cujos vetores ainda não foram visitados(cor = 1)
-def boundSomaArestasValidas2(problema):
-  soma = 0
-  # pprint(problema.grafo) # PRINTDEBUG
-  for idx, linha_adj in enumerate(problema.grafo):
-    maximo = 0
-    for peso in linha_adj[idx:]:
-      if (peso * problema.cores[idx] > maximo):
-        maximo = peso
-
-    soma += maximo
-  
-  # print("***", soma) # PRINTDEBUG
-  return soma 
-
-
-def buscaProdundidadeBB2(problema, vertice, total, caminho):
-  # Adiciona 'vertice' a caminho.
-  caminho.append(vertice)
-  problema.nosArvore += 1
-  
-  
-  # Caso exita aresta entre vertice atual e vertice 0 (início), uma solução viável foi encontrada.
-  if( vertice == 0 ):
-    # print(f"---- {total} ----", *[x+1 for x in caminho]) # PRINTDEBUG
-
-    #Caso solução viável seja maxímal até o momento, armazenar caminho e tamanho.
-    if (total > problema.tamanhoMaiorCicloSimples):
-      problema.tamanhoMaiorCicloSimples = total
-      problema.maiorCicloISmples = caminho.copy()
-
-    caminho.pop()
-    return  
-
-  # Bloqueia visitas à 'vertice' (cores[vertice] = 0), garantindo ciclo simples.
-  problema.mudarCor(vertice, 0)
-
-  # Percorrer linha da matriz correspondente a 'vertice'
-  for idx_vizinho, peso in enumerate(problema.grafo[vertice]):
-    # Caso exista aresta e vetor não foi visitado.
-    if((peso>0) and problema.cores[idx_vizinho]):
-      
-      # Caso caso não seja possível ultrapassar valor atual de caminho máximo, retornar(poda)
-      if(boundSomaArestasValidas2(problema) < problema.tamanhoMaiorCicloSimples):
-        # pprint(problema.grafo) # PRINTDEBUG
-        # print('\n') # PRINTDEBUG
-        problema.mudarCor(vertice, 0)
-        caminho.pop()
-        return
-      problema.removeAresta(vertice, idx_vizinho)
-      total += peso
-      buscaProdundidadeBB2(problema, idx_vizinho, total, caminho)
       total -= peso
       problema.adicionaAresta(vertice, idx_vizinho, peso)
 
@@ -184,6 +129,8 @@ def resolverProblema(problema):
   #####################
 
   print("------ Classico --------") # PRINTDEBUG
+  # problema.imprimeProblema() # PRINTDEBUG
+
 
   problema.tempo = datetime.now()
   problema.nosArvore = 1
@@ -202,16 +149,21 @@ def resolverProblema(problema):
   print(*[x+1 for x in problema.maiorCicloISmples])
   print(f"nos_arvore: {problema.nosArvore}\ntempo: {problema.tempo}", file=sys.stderr)
 
+  # problema.imprimeProblema() # PRINTDEBUG
   print("---------------------") # PRINTDEBUG
 
 
   #############################
   # Problema Branch and Bound #
   #############################
+  problema.tamanhoMaiorCicloSimples = 0
+  problema.maiorCicloISmples = []
+  problema.cores = [1] * problema.vertices
 
   print("-------- BB --------") # PRINTDEBUG
+  # problema.imprimeProblema() # PRINTDEBUG
 
-  problema.tempo = datetime.now()
+  problema.tempobb = datetime.now()
   problema.nosArvore = 1
 
   # Percorrer linha da matriz correspondente a 'vertice'
@@ -221,37 +173,14 @@ def resolverProblema(problema):
       buscaProdundidadeBB(problema, idx_vizinho, peso, [0])
       problema.adicionaAresta(0, idx_vizinho, peso)
       
-  problema.tempo = datetime.now() - problema.tempo
+  problema.tempobb = datetime.now() - problema.tempobb
 
   print(problema.tamanhoMaiorCicloSimples)
   print(*[x+1 for x in problema.maiorCicloISmples])
-  print(f"nos_arvore: {problema.nosArvore}\ntempo: {problema.tempo}", file=sys.stderr)
+  print(f"nos_arvore: {problema.nosArvore}\ntempo: {problema.tempobb}", file=sys.stderr)
 
-  print("---------------------") # PRINTDEBUG
-
-  #############################
-  # Problema Branch and Bound #
-  #############################
-
-  print("-------- BB2 --------") # PRINTDEBUG
-
-  problema.tempo = datetime.now()
-  problema.nosArvore = 1
-
-  # Percorrer linha da matriz correspondente a 'vertice'
-  for idx_vizinho, peso in enumerate(problema.grafo[0]):
-    if(peso>0):
-      problema.removeAresta(0, idx_vizinho)      
-      buscaProdundidadeBB(problema, idx_vizinho, peso, [0])
-      problema.adicionaAresta(0, idx_vizinho, peso)
-      
-  problema.tempo = datetime.now() - problema.tempo
-
-  print(problema.tamanhoMaiorCicloSimples)
-  print(*[x+1 for x in problema.maiorCicloISmples])
-  print(f"nos_arvore: {problema.nosArvore}\ntempo: {problema.tempo}", file=sys.stderr)
-
-  print("---------------------") # PRINTDEBUG
+  # problema.imprimeProblema() # PRINTDEBUG
+  print("---------------------") # PRINTDEBUG  
 
 def main():
   #Inicializando Problema
